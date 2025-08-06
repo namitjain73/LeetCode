b7df0242f0ca592558d8254247164888f60a7fd3
Namit jain <namitjain9838@gmail.com>
2025-08-06 18:59:40 +0530

    Create 2498-SmallestSubarraysWithMaximumBitwiseOr

---
+++ 2498-SmallestSubarraysWithMaximumBitwiseOr/2498-SmallestSubarraysWithMaximumBitwiseOr.java
@@ -0,0 +1,18 @@
+// Last updated: 8/6/2025, 6:59:36 PM
+class Solution {
+    List<Integer> list = new ArrayList<>();
+    public int[] smallestSubarrays(int[] nums) {
+        int n = nums.length;
+        int[] arr = new int[nums.length];
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = 1;
+            int x = nums[i];
+            for(int j = i - 1 ; j >= 0 && (nums[j] | x) != nums[j] ; j--){
+                nums[j] |= x;
+                arr[j] = i - j + 1;
+            }
+        }
+        return arr;
+    }
+
+}
