7ccc59c6a085920b6a1e9d36cd6628e2e45481e2
Namit jain <namitjain9838@gmail.com>
2025-08-06 18:59:12 +0530

    Create 2856-CountCompleteSubarraysInAnArray

---
+++ 2856-CountCompleteSubarraysInAnArray/2856-CountCompleteSubarraysInAnArray.java
@@ -0,0 +1,20 @@
+// Last updated: 8/6/2025, 6:59:08 PM
+class Solution {
+    public int countCompleteSubarrays(int[] nums) {
+        Map<Integer , Integer> map = new HashMap<>();
+        int left = 0;
+        int res = 0;
+        int k = (int) Arrays.stream(nums).distinct().count();
+
+        for(int i = 0 ; i < nums.length ; ++i){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            while(map.size() == k){
+                res += nums.length - i;
+                map.put(nums[left],map.get(nums[left]) -  1);
+                if(map.get(nums[left]) == 0) map.remove(nums[left]);
+                left++;
+            }
+        }
+        return res;
+    }
+}
