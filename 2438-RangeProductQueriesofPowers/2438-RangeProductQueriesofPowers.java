74be418c3f7a29e1202658bed258e79b3beeab57
Namit jain <namitjain9838@gmail.com>
2025-08-11 23:31:35 +0530

    File updated at 8/11/2025, 11:31:32 PM

--- 2438-RangeProductQueriesofPowers/2438-RangeProductQueriesofPowers.java
+++ 2438-RangeProductQueriesofPowers/2438-RangeProductQueriesofPowers.java
@@ -1,28 +1,23 @@
-// Last updated: 8/11/2025, 11:25:17 PM
+// Last updated: 8/11/2025, 11:31:32 PM
 class Solution {
     public int[] productQueries(int n, int[][] queries) {
         int[] arr = new int[queries.length];
-        List<Integer> list = new ArrayList<>();
         int mod = 1_000_000_007;
-        
+        List<Integer> list = new ArrayList<>();
         String s = Integer.toBinaryString(n);
-
-        for(int i = 0 ; i < s.length() ; i++){
+        for(int i = 0 ; i < s.length(); i++){
             int x = n & (1 << i);
-            if( x != 0){
-                list.add(x);
-            }
+            if(x != 0) list.add(x);
         }
 
-        // System.out.println(list);
-        for(int i = 0 ; i < queries.length ; i++){
+        for(int i = 0 ; i<queries.length ; i++){
             long p = 1;
             for(int j = queries[i][0] ; j <= queries[i][1] ; j++){
-                p = ((p * list.get(j))%mod)%mod;
+                p = (p * (list.get(j)) %mod ) %mod;
             }
-            arr[i] = (int)(p % mod);
+            arr[i] = (int)p;
         }
         return arr;
-
+        
     }
 }
