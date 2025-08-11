0f6cbe05471c4656a1e4d84a454325c4ed7d0be8
Namit jain <namitjain9838@gmail.com>
2025-08-11 23:25:21 +0530

    File updated at 8/11/2025, 11:25:17 PM

--- 2438-RangeProductQueriesofPowers/2438-RangeProductQueriesofPowers.java
+++ 2438-RangeProductQueriesofPowers/2438-RangeProductQueriesofPowers.java
@@ -1,4 +1,4 @@
-// Last updated: 8/11/2025, 11:24:56 PM
+// Last updated: 8/11/2025, 11:25:17 PM
 class Solution {
     public int[] productQueries(int n, int[][] queries) {
         int[] arr = new int[queries.length];
@@ -14,7 +14,7 @@ class Solution {
             }
         }
 
-        System.out.println(list);
+        // System.out.println(list);
         for(int i = 0 ; i < queries.length ; i++){
             long p = 1;
             for(int j = queries[i][0] ; j <= queries[i][1] ; j++){
