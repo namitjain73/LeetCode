7b3f836950c2a3cca53f9f069361feee3558be52
Namit jain <namitjain9838@gmail.com>
2025-08-13 11:40:05 +0530

    File updated at 8/13/2025, 11:40:01 AM

--- 968-BinaryTreeCameras/968-BinaryTreeCameras.java
+++ 968-BinaryTreeCameras/968-BinaryTreeCameras.java
@@ -1,4 +1,4 @@
-// Last updated: 8/13/2025, 11:34:50 AM
+// Last updated: 8/13/2025, 11:40:01 AM
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -16,27 +16,25 @@
  */
 class Solution {
     int camera = 0;
+    public int minCameraCover(TreeNode root) {
+        int c = minCamera(root);
+        if(c == -1 ) camera++;
+        return camera;
+    }
+
     public int minCamera(TreeNode root){
         if(root == null) return 0;
+
+
         int left = minCamera(root.left);
         int right = minCamera(root.right);
 
-        if(left == -1 || right == -1){ //is node camers ki need hai
+        if(left == -1 || right == -1) {
             camera++;
-            return 1; // camera setupkiya hai
+            return 1;
         }
-        else if(left == 1 || right == 1){// inme se kisis ek ke pass camers hai so is node par jarurat nahi hai
-        return 0;// iska mtlb mei coverd hu
-        }
-        else{ // aan mere pass camera nahi hai  so ujhe jarurat hai
-            return -1;
-        }
-
-
-    }
-    public int minCameraCover(TreeNode root) {
-         int c = minCamera(root);
-         if(c == -1) camera++;
-         return camera;
+        else if(left == 1 || right == 1) return 0;
+        else return -1;
+        // return root;
     }
 }
