c498963606658c31d1cdfc9d787537fb845fcc11
Namit jain <namitjain9838@gmail.com>
2025-08-13 11:25:51 +0530

    File updated at 8/13/2025, 11:25:46 AM

--- 326-PowerofThree/326-PowerofThree.java
+++ 326-PowerofThree/326-PowerofThree.java
@@ -1,14 +1,17 @@
-// Last updated: 8/13/2025, 11:12:00 AM
+// Last updated: 8/13/2025, 11:25:46 AM
 class Solution {
     public boolean isPowerOfThree(int n) {
         if(n == Integer.MAX_VALUE) return false;
-        
-        for(int i = 0 ; i < n;i++){
+
+
+        int i = 0;
+        while(n >= Math.pow(3,i)){
             int pow = (int)Math.pow(3,i);
             if(n == pow) return true;
-            else if(n < pow) return false;
+            i++;
         }
-
         return false;
+
+
     }
 }
