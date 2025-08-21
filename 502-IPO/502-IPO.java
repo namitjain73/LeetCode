3b790fbc03c2f4933c9d538bef368cc3fb34babf
Namit jain <namitjain9838@gmail.com>
2025-08-21 11:42:06 +0530

    Create 502-IPO

---
+++ 502-IPO/502-IPO.java
@@ -0,0 +1,30 @@
+// Last updated: 8/21/2025, 11:42:03 AM
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profit, int[] capital) {
+        int[][] arr = new int[profit.length][2];
+		for(int i = 0 ; i < arr.length ; i++) {
+			arr[i][0] = capital[i];
+			arr[i][1] = profit[i];
+		}
+        return IPO(arr,k,w);
+    }
+    public static int IPO(int[][] arr , int k ,int w) {
+		Arrays.sort(arr , (a,b) -> Integer.compare(a[0],b[0]));
+		PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(b[1], a[1]));
+		
+		for(int i = 0 ; i < arr.length ; ) {
+			while(i < arr.length && w >= arr[i][0]) {
+				pq.add(arr[i]);
+				i++;
+			}
+			if(pq.isEmpty() || k ==0) return w;
+			w += pq.poll()[1];
+			k--;
+		}
+        while(k-- > 0 && !pq.isEmpty()){
+            w  += pq.poll()[1]; 
+        }
+		return w;
+		
+	}
+}
