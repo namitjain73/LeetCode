842eaa461a160ea22799a4850938b783d64b711c
Namit jain <namitjain9838@gmail.com>
2025-08-26 17:07:11 +0530

    File updated at 8/26/2025, 5:07:06 PM

--- 3000-MaximumAreaofLongestDiagonalRectangle/3000-MaximumAreaofLongestDiagonalRectangle.java
+++ 3000-MaximumAreaofLongestDiagonalRectangle/3000-MaximumAreaofLongestDiagonalRectangle.java
@@ -1,20 +1,19 @@
-// Last updated: 8/26/2025, 5:04:38 PM
+// Last updated: 8/26/2025, 5:07:06 PM
 class Solution {
     public int areaOfMaxDiagonal(int[][] arr) {
 
         double max = 0.0;
-        int idx = 0;
-        int n = arr.length;
         int res = 0;
-        for (int[] rect : arr) {
-            int l = rect[0], b = rect[1];
-            double d = Math.sqrt((double)l * l + (double)b * b);
-            
-            if (d > max) {
+        int n = arr.length;
+        for(int[] ar : arr){
+            int l = ar[0];
+            int r = ar[1];
+            double d = Math.sqrt(l*l+r*r);
+            if(max < d){
                 max = d;
-                res = l * b;
-            } else if (d == max) {
-                res = Math.max(res, l * b);
+                res = l*r;
+            }else if(d == max){
+                res = Math.max(res,l*r);
             }
         }
         return res;
