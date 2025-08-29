3d25e17572140888296e2f9e1238803dd30fa7ca
Namit jain <namitjain9838@gmail.com>
2025-08-29 14:34:33 +0530

    Create 15-3Sum

---
+++ 15-3Sum/15-3Sum.java
@@ -0,0 +1,27 @@
+// Last updated: 8/29/2025, 2:34:24 PM
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> ll = new HashSet<>();
+        Arrays.sort(nums);
+        for(int i = 0 ; i < nums.length-1; i++){
+            int lo = i + 1;
+            int hi =  nums.length-1;
+            while(lo < hi){
+                int sum = nums[i] + nums[lo]  + nums[hi];
+                if(sum == 0){
+                    ll.add(Arrays.asList(nums[i],nums[lo],nums[hi]));
+                    lo++;
+                    hi--;
+                }
+                else if(sum < 0){
+                    lo++;
+                }
+                else{
+                    hi--;
+                }
+            }
+
+        }
+        return new ArrayList<>(ll);
+    }
+}
