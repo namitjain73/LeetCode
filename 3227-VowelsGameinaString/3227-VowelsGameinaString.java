4bedc4c1f5f91bbbf2b5f5fe6dfc2ef27c94fc99
Namit jain <namitjain9838@gmail.com>
2025-09-12 12:45:10 +0530

    File updated at 9/12/2025, 12:45:09 PM

--- 3227-VowelsGameinaString/3227-VowelsGameinaString.java
+++ 3227-VowelsGameinaString/3227-VowelsGameinaString.java
@@ -1,13 +1,7 @@
-// Last updated: 9/12/2025, 12:41:29 PM
+// Last updated: 9/12/2025, 12:45:09 PM
 class Solution {
     public boolean doesAliceWin(String s) {
-        int count = 0;
-        for(char ch : s.toCharArray()){
-            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch== 'u'){
-                return true;
-            }
-        }
-        return false;
+        return s.chars().anyMatch(ch -> "aeiou".indexOf(ch) >= 0);
         
     }
 }
