8b5443b9f8659b8e869cb783cb140abbd60805c8
Namit jain <namitjain9838@gmail.com>
2025-09-16 23:33:30 +0530

    File updated at 9/16/2025, 11:33:28 PM

--- 2197-ReplaceNon-CoprimeNumbersinArray/2197-ReplaceNon-CoprimeNumbersinArray.java
+++ 2197-ReplaceNon-CoprimeNumbersinArray/2197-ReplaceNon-CoprimeNumbersinArray.java
@@ -1,20 +1,30 @@
-// Last updated: 9/16/2025, 11:27:20 PM
+// Last updated: 9/16/2025, 11:33:28 PM
 class Solution {
     public List<Integer> replaceNonCoprimes(int[] nums) {
-        Stack<Integer>  st = new Stack<>();
-        
-
-
+        // Stack<Integer>  st = new Stack<>();
+        // for(int i = 0 ; i < nums.length ; i++){
+        //     int curr = nums[i];
+        //     while(!st.isEmpty() && gcd(curr,st.peek()) > 1){
+        //         curr = (int)lcm(curr,st.pop());
+        //     }
+        //     st.push(curr);
+        // }
+        // List<Integer> ll = new ArrayList<>();
+        // for(int i : st){
+        //     ll.add(i);
+        // }
+        List<Integer> ll = new ArrayList<>();
+        int j = 0;
         for(int i = 0 ; i < nums.length ; i++){
             int curr = nums[i];
-            while(!st.isEmpty() && gcd(curr,st.peek()) > 1){
-                curr = (int)lcm(curr,st.pop());
+            while(j > 0 && gcd(curr,ll.get(j-1)) > 1){
+                curr = (int)lcm(curr,ll.get(j-1));
+                ll.remove(j-1);
+                j--;
             }
-            st.push(curr);
-        }
-        List<Integer> ll = new ArrayList<>();
-        for(int i : st){
-            ll.add(i);
+            // st.push(curr);
+            ll.add(curr);
+            j++;
         }
         return ll;
     }
