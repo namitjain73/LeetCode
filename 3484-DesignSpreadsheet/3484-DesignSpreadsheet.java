8071d7acbd19355504c50ea442226ebc2c1d8951
Namit jain <namitjain9838@gmail.com>
2025-09-19 15:55:13 +0530

    File updated at 9/19/2025, 3:55:12 PM

--- 3484-DesignSpreadsheet/3484-DesignSpreadsheet.java
+++ 3484-DesignSpreadsheet/3484-DesignSpreadsheet.java
@@ -1,4 +1,4 @@
-// Last updated: 9/19/2025, 3:52:25 PM
+// Last updated: 9/19/2025, 3:55:12 PM
 class Spreadsheet {
 int[][] arr;
     public Spreadsheet(int rows) {
@@ -19,12 +19,15 @@ int[][] arr;
     
     public int getValue(String formula) {
         String s1 = "";
+        boolean s1b = true;
         String s2 = "";
+        boolean s2b = true;
         int i = 0;
         while(i < formula.length()){
             char ch = formula.charAt(i);
             if(ch == '+') break;
             else if(ch != '='){
+                if(!((ch-'0') >= 0 && (ch-'0') <= 9)) s1b = false;
                 s1 += Character.toString(ch);
             }
             i++;
@@ -33,24 +36,24 @@ int[][] arr;
         while(i < formula.length()){
             char ch = formula.charAt(i);
             s2 += Character.toString(ch);
+            if(!((ch-'0') >= 0 && (ch-'0') <= 9)) s2b = false;
             i++;
         }
 
         int sum = 0;
 
-        if(Character.isDigit(s1.charAt(0))) sum += Integer.parseInt(s1);
+        if(s1b) sum += Integer.parseInt(s1);
         else{
             char ch = s1.charAt(0);
             int n = Integer.parseInt(s1.substring(1));
             sum += arr[n-1][ch-'A'];
         }
-        if(Character.isDigit(s2.charAt(0))) sum += Integer.parseInt(s2);
+        if(s2b) sum += Integer.parseInt(s2);
         else{
             char ch = s2.charAt(0);
             int n = Integer.parseInt(s2.substring(1));
             sum += arr[n-1][ch-'A'];
         }
-        System.out.println(s1 + " " + s2);
         return sum;
     }
 }
