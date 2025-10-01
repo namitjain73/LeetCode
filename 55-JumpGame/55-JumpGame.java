0b017797e2f04a97cb89a0c91527664053e1a367
Namit jain <namitjain9838@gmail.com>
2025-10-01 14:22:48 +0530

    File updated at 10/1/2025, 2:22:47 PM

--- 55-JumpGame/55-JumpGame.java
+++ 55-JumpGame/55-JumpGame.java
@@ -1,11 +1,23 @@
-// Last updated: 10/1/2025, 2:17:37 PM
+// Last updated: 10/1/2025, 2:22:47 PM
 class Solution {
-    public boolean canJump(int[] nums) {
-        int jump = 0;
-        for(int i = 0 ; i < nums.length ; i++){
-            if(jump < i) return false;
-            jump = Math.max(jump,i+nums[i]);
+    static{
+        System.gc();
+        for(int i = 0; i < 500; i++){
+            new Solution().canJump(new int[] {2, 3, 1, 1, 4});
         }
-        return true;
+    }
+    
+    public boolean canJump(int[] nums) {
+        if (nums == null || nums.length == 0) return false;
+        int n = nums.length;
+        int maxReach = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i > maxReach) return false;
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (maxReach >= n - 1) return true;
+        }
+
+        return maxReach >= n - 1;
     }
 }
