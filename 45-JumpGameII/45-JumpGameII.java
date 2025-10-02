c772f6470a1540e62f50aedab9cbe9618c73159e
Namit jain <namitjain9838@gmail.com>
2025-10-03 01:57:01 +0530

    File updated at 10/3/2025, 1:57:00 AM

--- 45-JumpGameII/45-JumpGameII.java
+++ 45-JumpGameII/45-JumpGameII.java
@@ -1,20 +1,17 @@
-// Last updated: 10/3/2025, 1:55:12 AM
+// Last updated: 10/3/2025, 1:57:00 AM
 class Solution {
     public int jump(int[] nums) {
+        if(nums.length == 1) return 0;
+        int rc = 0;
         int jump = 0;
-        int curr= 0;
-        int fre = 0;
-
-        for(int i = 0 ; i < nums.length - 1; i++){
-            fre = Math.max(fre , i+nums[i]);
-
+        int curr = 0;
+        for(int i = 0 ; i < nums.length-1 ; i++){
+            rc = Math.max(rc,i+nums[i]);
             if(i == curr){
                 jump++;
-                curr = fre;
+                curr = rc;
             }
         }
-
         return jump;
-        
     }
 }
