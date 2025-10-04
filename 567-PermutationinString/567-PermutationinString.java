896013570b936957dc6b6e56f8bd8d47c1cea208
Namit jain <namitjain9838@gmail.com>
2025-10-05 02:22:14 +0530

    File updated at 10/5/2025, 2:22:11 AM

--- 567-PermutationinString/567-PermutationinString.java
+++ 567-PermutationinString/567-PermutationinString.java
@@ -1,27 +1,40 @@
-// Last updated: 10/5/2025, 2:20:57 AM
+// Last updated: 10/5/2025, 2:22:11 AM
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        Map<Character , Integer> map= new HashMap<>();
-        int n = s2.length();
+        if (s1.length() > s2.length()) return false;
+        
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        
+        // build frequency for s1
+        for (char c : s1.toCharArray()) {
+            freq1[c - 'a']++;
+        }
+        
         int k = s1.length();
-        for(char ch : s1.toCharArray()){
-            map.put(ch,map.getOrDefault(ch,0)+1);
+        
+        // first window
+        for (int i = 0; i < k; i++) {
+            freq2[s2.charAt(i) - 'a']++;
         }
-
-        for(int i = 0 ; i < n-k+1 ; i++){
-            if(map.containsKey(s2.charAt(i)) && present(map,s2.substring(i,i+k))) return true;
+        
+        if (matches(freq1, freq2)) return true;
+        
+        // sliding window
+        for (int i = k; i < s2.length(); i++) {
+            freq2[s2.charAt(i) - 'a']++;   // add new char
+            freq2[s2.charAt(i - k) - 'a']--; // remove old char
+            
+            if (matches(freq1, freq2)) return true;
         }
+        
         return false;
     }
-    public boolean present(Map<Character , Integer> map , String s){
-        Map<Character, Integer> copymap = new HashMap<>(map);
-        for(char ch : s.toCharArray()){
-            if(copymap.containsKey(ch)){
-                copymap.put(ch,copymap.getOrDefault(ch,0)-1);
-                if(copymap.get(ch) == 0) copymap.remove(ch);
-            }else return false;
+    
+    private boolean matches(int[] a, int[] b) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) return false;
         }
-
-        return copymap.isEmpty();
+        return true;
     }
 }
