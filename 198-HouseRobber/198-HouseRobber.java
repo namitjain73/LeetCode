55d746a9b3f3535cc4e3ffa0f66f022e58ac20c5
Namit jain <namitjain9838@gmail.com>
2025-10-04 23:26:18 +0530

    File updated at 10/4/2025, 11:26:15 PM

--- 198-HouseRobber/198-HouseRobber.java
+++ 198-HouseRobber/198-HouseRobber.java
@@ -1,64 +1,22 @@
-// Last updated: 9/3/2025, 11:28:51 AM
+// Last updated: 10/4/2025, 11:26:15 PM
 class Solution {
-    public int rob(int[] arr) {
-        if(arr.length < 2) return arr[arr.length-1];
-		int n = arr.length;
-		int[] dp = new int[arr.length+1];
-		dp[0] = arr[0];
-		dp[1] = Math.max(arr[0],arr[1]);
-		for(int i = 2; i < arr.length ; i++) {
-			dp[i] = Math.max(dp[i-1], dp[i-2]+ arr[i]);
-		}
-		return dp[n-1];
-	}
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n < 2){
+            return nums[0];
+        }
+        int[] dp = new int[n+1];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+        int max = 0;
+        for(int i =2 ; i < n ;i++){
+            int take = dp[i-2] + nums[i];
+            int skip = dp[i-1];
+            dp[i] = Math.max(take,skip);
+        }
+
+
+        return dp[n-1];
+        
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
