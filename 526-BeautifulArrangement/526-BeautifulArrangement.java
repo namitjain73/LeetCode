48a6d895a019c864f4d69b6fac65170cfd879e97
Namit jain <namitjain9838@gmail.com>
2025-10-05 18:17:01 +0530

    File updated at 10/5/2025, 6:16:59 PM

--- 526-BeautifulArrangement/526-BeautifulArrangement.java
+++ 526-BeautifulArrangement/526-BeautifulArrangement.java
@@ -1,17 +1,14 @@
-// Last updated: 10/5/2025, 6:16:17 PM
+// Last updated: 10/5/2025, 6:16:59 PM
 class Solution {
     int ans;
     public int countArrangement(int n) {
         ans =  0;
         List<Integer> list = new ArrayList<>();
-        List<List<Integer>> ll = new ArrayList<>();
-        solve(n ,ll,list);
-        System.out.println(ll);
+        solve(n ,list);
         return ans;
     }
-    public void solve(int n , List<List<Integer>> ll , List<Integer> list){
+    public void solve(int n , List<Integer> list){
         if(list.size() == n){
-            ll.add(new ArrayList<>(list));
             ans++;
             return;
         }
@@ -20,7 +17,7 @@ class Solution {
             if(list.contains(i)) continue;
             if((list.size()+1)%i == 0 || i%(list.size()+1)==0){
                 list.add(i);
-                solve(n,ll,list);
+                solve(n,list);
                 list.remove(list.size()-1);
             }
         }
