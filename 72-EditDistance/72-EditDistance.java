2c060baf2f0c231ee9c582d3c60a3c63365f3b92
Namit jain <namitjain9838@gmail.com>
2025-10-06 11:53:03 +0530

    File updated at 10/6/2025, 11:53:02 AM

--- 72-EditDistance/72-EditDistance.java
+++ 72-EditDistance/72-EditDistance.java
@@ -1,8 +1,8 @@
-// Last updated: 10/6/2025, 11:49:47 AM
+// Last updated: 10/6/2025, 11:53:02 AM
 class Solution {
     public int minDistance(String word1, String word2) {
         int[][] dp =new int[word1.length()][word2.length()];
-        for(int[] d : dp) Arrays.fill(d,-1);
+        // for(int[] d : dp) Arrays.fill(d,-1);
         return solver(word1,word2,0,0,dp);
     }
     public int solver(String s1 , String s2 , int i , int j,int[][] dp){
@@ -12,7 +12,7 @@ class Solution {
         if(j == s2.length()){
             return s1.length() - i;
         }
-        if(dp[i][j] != -1) return dp[i][j];
+        if(dp[i][j] != 0) return dp[i][j];
 
         int ans = 0;
         if(s1.charAt(i) != s2.charAt(j)){
